1f1243ed7216ba9d805ff86a3d5e60e286ff1a0c
Artem Sapegin <artem@sapegin.ru>
2013-10-07 22:33:46 +0400

    jQuery plugin generator.

---
+++ src/jqplugin/index.coffee
@@ -0,0 +1,29 @@
+# Creates new jQuery plugin.
+
+'use strict'
+
+base = require '../base'
+
+module.exports = class Generator extends base
+
+Generator::askFor = ->
+	done = @async()
+	prompts = [
+		name: 'name'
+		message: 'Plugin name'
+	]
+
+	@prompt prompts, (err, props) =>
+		return (@emit 'error', err)  if err
+
+		@name = @_.capitalize props.name
+		@filename = (props.name.replace /[^a-z]/ig, '').toLowerCase()
+		@cls = @_.classify props.name
+		@method = (@cls.charAt 0).toLowerCase() + @cls.slice(1)
+
+		done()
+
+Generator::files = ->
+	filepath = "jquery.#{@filename}.js"
+	@stopIfExists filepath
+	@template 'jqplugin.js', filepath
---
+++ templates/jqplugin.js
@@ -0,0 +1,50 @@
+/**
+ * <%= name %>
+ *
+ * Makes bla bla.
+ *
+ * @version 0.0.0
+ * @requires jQuery
+ * @author <%= authorName %>
+ * @copyright <%= (new Date).getFullYear() %> <%= authorName %>, <%= authorUrl %>
+ * @license MIT
+ */
+
+/*jshint browser:true, jquery:true, white:false, smarttabs:true, eqeqeq:true,
+         immed:true, latedef:false, newcap:true, undef:true */
+/*global define:false*/
+;(function(factory) {  // Try to register as an anonymous AMD module
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else {
+		factory(jQuery);
+	}
+}(function($) {
+	'use strict';
+
+	$.fn.<%= method %> = function(options) {
+		options = $.extend({}, $.fn.<%= method %>.defaults, options);
+
+		return this.each(function() {
+			// var elem = $(this);
+			new <%= cls %>($(this), options);
+		});
+	};
+
+	$.fn.<%= method %>.defaults = {
+	};
+
+	function <%= cls %>(container, options) {
+		this.container = container;
+		this.options = options;
+
+		this.init();
+	}
+
+	<%= cls %>.prototype = {
+		init: function() {
+			/* Magic begins here */
+		}
+	};
+
+}));
